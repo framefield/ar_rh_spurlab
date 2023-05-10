8097057274c7c0e97732950cd4ff766eb6e8eb87
pirx <andreas.rose@framefield.com>
2023-05-10 16:35:27 +0200

    refactoring

--- Assets/ff/ar_rh_spurlab/Calibration/Scripts/CalibrationController.cs
+++ Assets/ff/ar_rh_spurlab/Calibration/Scripts/CalibrationController.cs
@@ -69,29 +69,12 @@ namespace ff.ar_rh_spurlab
 
             if (m_PlacedMarkerObjects.Count == MAX_NUMBER_OF_REFERENCE_POINTS)
             {
-                //Vector3 locationOriginPp01 = new Vector3(-11.2624f, -2.74274f, -6.8056f);
-                //Vector3 locationOriginPp02 = new Vector3(-10.6223f, -2.36561f, -6.79544f);
-                //Vector3 locationOriginPp03 = new Vector3(-8.82212f, -2.12815f, -6.81208f);
-                //Vector3 locationOriginPp04 = new Vector3(-8.46449f, -4.83936f, -6.81208f);
-                //Vector3 locationOriginPp05 = new Vector3(-8.10017f, -7.60133f, -6.81208f);
-                //Vector3 locationOriginPp06 = new Vector3(-8.10017f, -7.60133f, -9.70552f);
-                //Vector3 locationOriginPp07 = new Vector3(-8.74312f, -2.72705f, -9.94253f);
+                Vector3[] pointsInLocationOrigin = { new Vector3(10.615f, 6.802f, 2.355f), new Vector3(8.448f, 6.809f, 4.812f), new Vector3(8.734f, 9.936f, 2.708f) };
+                Vector3[] pointsInXROrigin = { m_PlacedMarkerObjects[0].transform.position,
+                                               m_PlacedMarkerObjects[1].transform.position,
+                                               m_PlacedMarkerObjects[2].transform.position };
 
-                Vector3 locationOriginPp01 = new Vector3(10.615f, 6.802f, 2.355f);
-                Vector3 locationOriginPp02 = new Vector3(8.448f, 6.809f, 4.812f);
-                Vector3 locationOriginPp03 = new Vector3(8.734f, 9.936f, 2.708f);
-
-                Matrix4x4 locationOriginTMarker = Matrix4x4.TRS(locationOriginPp01, Quaternion.LookRotation(locationOriginPp01 - locationOriginPp03, locationOriginPp01 - locationOriginPp02), Vector3.one);
-
-
-                Vector3 xrOriginPp01 = m_PlacedMarkerObjects[0].transform.position;
-                Vector3 xrOriginPp02 = m_PlacedMarkerObjects[1].transform.position;
-                Vector3 xrOriginPp03 = m_PlacedMarkerObjects[2].transform.position;
-
-                Matrix4x4 xrOriginTMarker = Matrix4x4.TRS(xrOriginPp01, Quaternion.LookRotation(xrOriginPp01 - xrOriginPp03, xrOriginPp01 - xrOriginPp02), Vector3.one);
-
-                Matrix4x4 xrOriginTLocationOrigin = xrOriginTMarker * Matrix4x4.Inverse(locationOriginTMarker);
-                Debug.Log("xr T location: " + xrOriginTLocationOrigin.GetPosition().ToString() + ", " + xrOriginTLocationOrigin.rotation.ToString());
+                Matrix4x4 xrOriginTLocationOrigin = calculateTransfromFromeAToB(pointsInLocationOrigin, pointsInXROrigin);
                 if (m_LocationObject == null)
                 {
                     m_LocationObject = Instantiate(m_LocationPrefab, xrOriginTLocationOrigin.GetPosition(), xrOriginTLocationOrigin.rotation, m_XROrigin);
@@ -103,6 +86,17 @@ namespace ff.ar_rh_spurlab
                 }
             }
 
+        }
+
+        Matrix4x4 calculateTransfromFromeAToB(IList<Vector3> pointsInA, IList<Vector3> pointsInB)
+        {
+            if (pointsInA.Count != pointsInB.Count || pointsInA.Count != 3)
+                return Matrix4x4.identity;
+
+            Matrix4x4 aTPoints = Matrix4x4.TRS(pointsInA[0], Quaternion.LookRotation(pointsInA[0] - pointsInA[2], pointsInA[0] - pointsInA[1]), Vector3.one);
+            Matrix4x4 bTPoints = Matrix4x4.TRS(pointsInB[0], Quaternion.LookRotation(pointsInB[0] - pointsInB[2], pointsInB[0] - pointsInB[1]), Vector3.one);
+            Matrix4x4 bTA = bTPoints * Matrix4x4.Inverse(aTPoints);
+            return bTA;
         }
 
         protected override void OnPress(Vector3 position) => m_Pressed = true;
