e7a5f2cb5d088f2a71e888ea45ebfbabe4560a2a
Deniz Bicer <deniz.bicer@framefield.com>
2023-06-06 17:23:00 +0200

    fixed layout problem and created app menu prefab

---
+++ Assets/ff/ar_rh_spurlab/UI/AppMenuController.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace ff.ar_rh_spurlab.UI
+{
+    public class AppMenuController : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        }
+    }
+}
--- Assets/ff/ar_rh_spurlab/UI/Site-Ui/AvailableSitesUi.cs
+++ Assets/ff/ar_rh_spurlab/UI/Site-Ui/AvailableSitesUi.cs
@@ -1,17 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using ff.ar_rh_spurlab.Locations;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace ff.ar_rh_spurlab.UI.Site_Ui
 {
     public class AvailableSitesUi : MonoBehaviour
     {
-        [SerializeField]
-        private LayoutElement _layoutElement;
-
         [Header("Asset References")]
         [SerializeField]
         private AvailableSites _availableSites;
@@ -25,16 +18,7 @@ namespace ff.ar_rh_spurlab.UI.Site_Ui
             {
                 var siteUi = Instantiate(_siteUiPrefab, transform);
                 siteUi.SetSiteData(site);
-                _siteUis.Add(siteUi);
             }
         }
-
-        private void Update()
-        {
-            var height = _siteUis.Sum(siteUi => siteUi.GetComponent<RectTransform>().rect.height);
-            _layoutElement.minHeight = height;
-        }
-
-        private readonly List<SiteUi> _siteUis = new();
     }
 }
