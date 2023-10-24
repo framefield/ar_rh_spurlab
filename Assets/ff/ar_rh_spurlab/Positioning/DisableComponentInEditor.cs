7f8906b868eba26784c9ff1e3006d23be370ca7c
Thomas Mann <thomas.mann@framefield.com>
2023-10-24 14:03:11 +0200

    Disable XRAnchorManagement in Unity editor to avoid spamming the scene with debug objects

---
+++ Assets/ff/ar_rh_spurlab/Positioning/DisableComponentInEditor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace ff.ar_rh_spurlab.Positioning
+{
+    public class DisableComponentInEditor : MonoBehaviour
+    {
+        [SerializeField] private MonoBehaviour _component = default;
+
+        private void Awake()
+        {
+#if UNITY_EDITOR
+            _component.enabled = false;
+#endif
+        }
+
+    }
+}
